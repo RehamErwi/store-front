1a1629f3c428b37b4106e81eaf2b48a8d54b14eb
Reham <RehamErwi@gmail.com>
2023-02-27 22:34:37 +0300

    remove old code

--- src/types/order.type.ts
+++
@@ -1,7 +0,0 @@
-type Order = {
-    id: string;
-    user_id: string;
-    status: string;
-};
-
-export default Order;
--- src/types/orderDetails.type.ts
+++
@@ -1,8 +0,0 @@
-type OrderDetails = {
-    id: string;
-    product_id: string;
-    order_id: string;
-    quantity: number;
-};
-
-export default OrderDetails;
